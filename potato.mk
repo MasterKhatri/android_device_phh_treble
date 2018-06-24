fd35fa1e9f4c90fe9b7bfb423a6565571aef8ed7
MasterKhatri <akash.khatri2000@gmail.com>
2018-06-24 11:06:28 +0000

    Add potato in vendor path

--- potato.mk
+++ potato.mk
@@ -1,4 +1,4 @@
 $(call inherit-product, vendor/potato/config/common_full_phone.mk)
 $(call inherit-product, device/potato/sepolicy/common/sepolicy.mk)
--include vendor/lineage/build/core/config.mk
--include vendor/lineage/build/core/apicheck.mk
+-include vendor/potato/build/core/config.mk
+-include vendor/potato/build/core/apicheck.mk
